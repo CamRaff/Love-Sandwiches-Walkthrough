eb1a609263efb968504f6684812a82f48286a569
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-09-03 12:00:29 +0100

    Added function to add sales data to the sales worksheet

--- run.py
+++ run.py
@@ -23,6 +23,9 @@ print("Welcome to Love Sandwiches Data Automation")
 def get_sales_data():
     """
     Get sales figures input from the user
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -36,6 +39,8 @@ def get_sales_data():
         if validate_data(sales_data):
             print("Data is valid!")
             break
+    
+    return sales_data
 
 def validate_data(values):
     """
@@ -52,7 +57,19 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
+    
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
 
